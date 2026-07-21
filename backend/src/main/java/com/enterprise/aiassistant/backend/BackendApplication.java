4f7ad930ea3ec7d856a2fa1eeecf1bb0f4d05620
bao-ho-le <levo24k@gmail.com>
2026-07-22 03:01:11 +0700

    Stop tracking application.properties

--- backend/src/main/java/com/enterprise/aiassistant/backend/BackendApplication.java
+++ backend/src/main/java/com/enterprise/aiassistant/backend/BackendApplication.java
@@ -2,6 +2,8 @@ package com.enterprise.aiassistant.backend;
 
 import com.enterprise.aiassistant.backend.ai.embedding.config.GeminiProperties;
 import com.enterprise.aiassistant.backend.ai.vectorstore.config.QdrantProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,6 +15,7 @@ import org.springframework.retry.annotation.EnableRetry;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
